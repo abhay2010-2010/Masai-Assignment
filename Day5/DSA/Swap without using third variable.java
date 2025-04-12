861a443775da2fb25e496ef45103e093e1386f0c
abhay2010-2010 <aabhhayy2010@gmail.com>
2025-04-12 16:04:08 +0530

    day5

---
+++ Day5/DSA/Swap without using third variable.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int x=5;
+        int y=10;
+        x=x+y;
+        y=x-y;
+        x=x-y;
+        System.out.println(x+" "+y);
+    }
+}
