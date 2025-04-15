4f8507d36908ba54fc38b789e0afffda92fddd62
abhay2010-2010 <aabhhayy2010@gmail.com>
2025-04-15 13:19:48 +0530

    day6

--- day3/TodoApp/src/main.jsx
+++ day3/TodoApp/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import store from './components/redux/Store.jsx'
-// import Provider from "react-redux";
+import Provider from "react-redux";
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>  <App /></Provider>
