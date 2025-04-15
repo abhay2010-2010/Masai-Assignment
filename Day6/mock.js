b75dd005f48ea71dbc20d02ca9ca92d7ffc9e814
abhay2010-2010 <aabhhayy2010@gmail.com>
2025-04-15 13:16:25 +0530

    day6

--- Day6/mock.js
+++ Day6/mock.js
@@ -17,4 +17,62 @@
 // }
 // console.log(largenum);
 
+// let arr=[{fname:"aaa",lname:"rrr", age:11},{fname:"bbb",lname:"qqq",age:12}];
+// // expected={aaa_rrr:11,bbb_qqq:12};
+//  let ans=arr.reduce((acc,itm)=>{
+//     acc={...acc,[`${itm.fname}_${itm.lname}`]:`${itm.age}`}
+// return acc;
+//  },{});
 
+//  console.log(ans);
+
+const people = [
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 32 },
+  { name: "Charlie", age: 19 },
+
+  { name: "David", age: 47 },
+  { name: "Emily", age: 28 },
+  {name:"abhi",age:50}
+];
+
+let avgage =
+  people.reduce((curr, itm) => {
+    !curr.age|| curr.age<itm.age ? curr=itm :null;
+
+    return curr;
+  }, {}) ;
+// console.log(avgage);
+
+//input format
+const strings = ["apple", "banana", "apple", "orange", "banana", "pear", "apple"];
+
+//output format
+
+// {
+//   apple: 3,
+//   banana: 2,
+//   orange: 1,
+//   pear: 1
+// }
+let str=strings.reduce((curr,item)=>{
+    curr[item] = (curr[item] || 0) + 1;
+    return curr
+},{})
+
+// console.log(str)
+const numbers = [7, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597];
+const sum=numbers.reduce((acc,item)=>{
+    if( item%5===0){
+            acc+=item
+        
+    }
+    return acc;
+},0);
+// console.log(sum);
+const words = ["racecar", "hello", "deified", "world", "level", "programming", "radar", "civic", "javascript"];
+let pal=words.filter((itm,ind)=>{
+const rev=itm.split('').reverse().join('');
+return itm===rev;
+})
+ console.log(pal);
