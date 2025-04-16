d9b9efef4b9e86856f5d25cc049111d25d87eac6
abhay2010-2010 <aabhhayy2010@gmail.com>
2025-04-16 09:37:57 +0530

    day6

---
+++ Day7/mock.js
@@ -0,0 +1,9 @@
+const products = [
+    { name: "iPhone", price: 999 },
+    { name: "MacBook", price: 1499 },
+    { name: "iPad", price: 799 },
+    { name: "Apple Watch", price: 399 },
+    { name: "AirPods", price: 249 }
+  ];
+
+  
